793b5ae05f74d9c7249d77a6e701160261fd6cae
錦駿 鄭 <dog80630tom@gmail.com>
2019-11-16 18:41:45 +0800

    加入專案檔案。

---
+++ vegetable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vegetable.Startup))]
+namespace vegetable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
