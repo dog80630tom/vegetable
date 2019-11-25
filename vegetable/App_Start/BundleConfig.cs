9d23fa4b7deec6be83c0a65fe0ef8c3a7da0b0b7
錦駿 鄭 <dog80630tom@gmail.com>
2019-11-25 18:14:59 +0800

    產品List完成Detail完成

--- vegetable/App_Start/BundleConfig.cs
+++ vegetable/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace vegetable
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery.dataTables.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -29,7 +29,7 @@ namespace vegetable
 
             bundles.Add(new StyleBundle("~/Content/adminCss").Include(
 
-                      "~/Content/Css/sb-admin-2.min.css"));
+                      "~/Content/Css/sb-admin-2.min.css", "~/content/jquery.dataTables.min.css"));
 
         }
     }
