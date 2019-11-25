82e92d42a2ef89cc39b656cc603bad0873d0a934
Sean <wkus963@gmail.com>
2019-11-25 18:54:31 +0800

    產品頁初版

--- vegetable/Controllers/FrontEndController.cs
+++ vegetable/Controllers/FrontEndController.cs
@@ -33,5 +33,10 @@ namespace vegetable.Controllers
         {
             return View();
         }
+
+        public ActionResult ProductIndex ()
+        {
+            return View();
+        }
     }
 }
