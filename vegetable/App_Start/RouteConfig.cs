ad7acb32cad4cc82d8c7c176a063aa0a09a62f5a
Sean <wkus963@gmail.com>
2019-12-05 12:59:12 +0800

    增加耳環 ,showProduct路由， 分類頁增加圖片

--- vegetable/App_Start/RouteConfig.cs
+++ vegetable/App_Start/RouteConfig.cs
@@ -11,12 +11,38 @@ namespace vegetable
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //show product 
+            routes.MapRoute(
+              name: "ShowProducts",
+              url: "Products/{SearchCondition}",
+              defaults: new
+              {
+                  controller = "FrontEnd",
+                  action = "ShowProducts",
+                  SearchCondition = UrlParameter.Optional
+              }
+          );
+
+            //耳環
+            routes.MapRoute(
+               name: "Earrings",
+               url: "Products/Earrings/{id}",
+               defaults: new
+               {
+                   controller = "FrontEnd",
+                   action = "ProductIndex",
+                   id = UrlParameter.Optional
+               }
+           );
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
                name: "HomePage",
                url: "{controller}/{action}/{id}",
                defaults: new { controller = "FrontEnd", action = "Index", id = UrlParameter.Optional }
            );
+
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
