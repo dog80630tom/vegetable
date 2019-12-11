0118a4860cadbac9a25e8200121c55e0eec36398
Sean <wkus963@gmail.com>
2019-12-11 19:52:28 +0800

    把購物車table 改寫入order detail, 並修正寫入後跟order table的關聯

--- vegetable/Controllers/FrontEndController.cs
+++ vegetable/Controllers/FrontEndController.cs
@@ -99,20 +99,67 @@ namespace vegetable.Controllers
         //[ValidateAntiForgeryToken]
         //改成ajax之前的寫法
         //public ActionResult AddCart ([Bind(Include = "CartID,MemberID,ProductID,Quantity")] CartDetail cart)
-        public ActionResult AddCart (CartDetail cart)
+        public void CheckOrder (OrderDetail cart)
         {
             if (ModelState.IsValid)
             {
                 using (ItemContext item = new ItemContext())
                 {
-                    item.CartDetails.Add(cart);
-                    item.SaveChanges();
-                    //無法到檢視??
-                    return RedirectToAction("Cart");
+                    // 確認這個memberID 在資料庫目前有多少orders
+                    // orders 可能是 null || 一筆 || 多筆
+                    var orders = from o in item.Orders
+                                 where o.MemberID == cart.MemberID
+                                 select o;
+
+                    // if true 此會員目前沒下過任何訂單
+                    if (orders.ToList().Count() == 0)
+                    {
+                        //如果orders沒資料, 創建一筆order, 並寫入資料庫
+                        Order newOrder = NewOrder(item, cart);
+                        AddCart(newOrder, cart, item);
+                    }
+                    // if true 此會員已有訂單 但不確定是否結帳
+                    else if (orders.ToList().Count() != 0)
+                    {
+                        // 如果DeliverName == null 表示未結帳
+                        // 此會員若有未結帳的訂單 理論上 只會有1筆
+                        // 判斷此會員的所有訂單是否都已結帳
+                        bool hasNotCheckoutOrders = orders.All(x => x.DeliverName != null);
+                        //都已經結帳 產生一筆新訂單
+                        if (hasNotCheckoutOrders)
+                        {
+                            Order newOrder = NewOrder(item, cart);
+                            AddCart(newOrder, cart, item);
+                        }
+                        //有尚未結帳的訂單, 新增的購物車可以繼續寫入
+                        else
+                        {
+                            //理論上未結帳訂單最多一筆
+                            var notCheckoutOrder = orders.Where(x => x.DeliverName == null).FirstOrDefault();
+                            cart.OrderID = notCheckoutOrder.OrderID;
+                            AddCart(notCheckoutOrder, cart, item);
+                        }
+                    }
                 }
             }
-            return View();
         }
+
+        //創建一筆新order
+        public Order NewOrder(ItemContext item, OrderDetail cart) {
+            Order newOrder = new Order();
+            newOrder.MemberID = cart.MemberID;
+            item.Orders.Add(newOrder);
+            item.SaveChanges();
+            return newOrder;
+        }
+
+        public void AddCart (Order order , OrderDetail cart, ItemContext item)
+        {
+            cart.OrderID = order.OrderID;
+            item.OrderDetails.Add(cart);
+            item.SaveChanges();
+        }
+
 
 
         public ActionResult MemberPageAddress ()
