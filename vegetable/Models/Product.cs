0353f613df1e45f7471122f8e26c8af844804a4a
Ricky <ricky19951011@gmail.com>
2019-12-16 10:18:33 +0800

    登入失敗訊息修改、WISHLIST(未完成

--- vegetable/Models/Product.cs
+++ vegetable/Models/Product.cs
@@ -12,7 +12,7 @@ namespace vegetable.Models
          public int ProductID{get; set;}
          public int CategoryID{get; set;}
          public string ProductName{get; set;}
-         public string CategoryName{get; set;}
+       
          public string ProductDescription{get; set;}
          public int UnitsInStock{get; set;}
          public decimal ProductPrice { get; set; }
