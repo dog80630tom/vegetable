365fd9ab4fa3ba6dd6c26a9ee21c30b9c6041b55
Sean <wkus963@gmail.com>
2020-01-11 00:05:48 +0800

    修復合併錯誤

--- vegetable/Global.asax.cs
+++ vegetable/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Http.Cors;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Thinktecture.IdentityModel.Http.Cors.Mvc;
 
 namespace vegetable
 {
